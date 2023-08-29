e1d6d9ce24da58c9500fa174e760e072239956cf
Surya Prakash Gupta <113275807+Surya-explorer@users.noreply.github.com>
2023-08-29 23:46:27 +0530

    Add files via upload
    Initial Commit

---
+++ index.js
@@ -0,0 +1,20 @@
+gsap.from("#tt1", {
+    opacity: 0,
+    x: -400,
+    duration: 2,
+    ease: "power4.out",
+    delay: 1 
+  });
+  gsap.from("#tt2", {
+    opacity: 0,
+    y: 500,
+    duration: 2,
+    ease: "power4.out",
+    delay: 1 
+  });gsap.from("#tt3", {
+    opacity: 0,
+    x: 400,
+    duration: 2,
+    ease: "power4.out",
+    delay: 1 
+  });
